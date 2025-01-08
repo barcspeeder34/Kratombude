71df2c25ccde428f67b970e23eeb180f0f7bce4e
barcspeeder34 <barcspeeder34@yahoo.com>
2025-01-08 22:22:16 +0100

    Update create-payment-intent.js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY1);
 
 
 export default async function handler(req, res) {
